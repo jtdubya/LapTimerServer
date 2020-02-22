4343a9f66f79b876b8fb0a00610cd4bced561f3a
John Wilson <john_wilson@trimble.com>
2020-02-21 18:35:42 -0700

    Initial commit - mosly template code with some initial lib classes

---
+++ Lib/Lap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lib
+{
+    public class Lap
+    {
+        private readonly int m_lapNumber;
+        private readonly TimeSpan m_lapTime;
+
+        public Lap(int lapNumber, TimeSpan lapTime)
+        {
+            m_lapNumber = lapNumber;
+            m_lapTime = lapTime;
+        }
+
+        public int GetLapNumber()
+        {
+            return m_lapNumber;
+        }
+
+        public TimeSpan GetLatTime()
+        {
+            return m_lapTime;
+        }
+    }
+}
---
+++ Lib/LapTimer.cs
@@ -0,0 +1,42 @@
+﻿using Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppPrototype.Lib
+{
+    public class LapTimer
+    {
+        private readonly int m_id;
+        private int m_lapCount;
+        private List<Lap> m_laps;
+
+        public LapTimer(int id)
+        {
+            m_id = id;
+            m_lapCount = 0;
+            m_laps = new List<Lap>();
+        }
+
+        public void AddLap(TimeSpan time)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetLapCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TimeSpan GetTotalTime()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Lap GetFastestLap()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
