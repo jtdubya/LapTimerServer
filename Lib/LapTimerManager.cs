84c88254088531c9ea1493bc9a93dd9b5f74df81
John Wilson <john_wilson@trimble.com>
2020-02-22 09:45:04 -0700

    Implemented lap timer manager with TDD

---
+++ Lib/LapTimerManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace WebAppPrototype.Lib
+{
+    public class LapTimerManager
+    {
+        private int m_nextId;
+        private Dictionary<IPAddress, LapTimer> m_lapTimers;
+
+        public LapTimerManager()
+        {
+            m_nextId = 1;
+            m_lapTimers = new Dictionary<IPAddress, LapTimer>();
+        }
+
+        /// <summary>
+        /// Registers a lap timer by IP address
+        /// </summary>
+        /// <param name="lapTimerIpAddress">The IP address of the lap timer</param>
+        /// <returns>ID of lap timer or -1 if a timer with the provided IP address alread exists</returns>
+        public int RegisterLapTimer(IPAddress lapTimerIpAddress)
+        {
+            int currentId = m_nextId;
+            m_nextId++;
+
+            LapTimer lapTimer = new LapTimer(currentId);
+            bool added = m_lapTimers.TryAdd(lapTimerIpAddress, lapTimer);
+
+            if (!added)
+            {
+                currentId = -1;
+            }
+
+            return currentId;
+        }
+
+        public Dictionary<IPAddress, LapTimer> GetAllLapTimers()
+        {
+            return m_lapTimers;
+        }
+    }
+}
