41c63456ba7b641a7c146c9c22236078b6721cc9
John Wilson <john_wilson@trimble.com>
2020-03-12 10:29:35 -0600

    started laptimer message handler using another console app for the server so the test in manual for now

---
+++ LibUnitTests/LapTimerMessageHandlerIntegrationTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Net;
+using Xunit;
+using WebAppPrototype.Lib;
+
+namespace WebAppPrototype.LibUnitTests
+{
+    public class LapTimerMessageHandlerIntegrationTests : IDisposable
+    {
+        private readonly LapTimerMessageHandler m_LapTimerMessageHandler;
+
+        public LapTimerMessageHandlerIntegrationTests()
+        {
+            m_LapTimerMessageHandler = new LapTimerMessageHandler();
+        }
+
+        public void Dispose()
+        {
+            m_LapTimerMessageHandler.Dispose();
+        }
+
+        [Fact]
+        public void SendRaceStartNotification()
+        {
+            string ipAddress = "127.0.0.1";
+            var message = m_LapTimerMessageHandler.SendRaceStartNotification(IPAddress.Parse(ipAddress), 5000);
+            message.Wait();
+            // assert ...
+        }
+    }
+}
