09c17dc38ad1a7d7670fbffd644674d4cb21ed04
John Wilson <john_wilson@trimble.com>
2020-03-12 10:29:35 -0600

    started laptimer message handler using another console app for the server so the test in manual for now

--- Lib/LapTimerMessageHandler.cs
+++ Lib/LapTimerMessageHandler.cs
@@ -1,6 +1,38 @@
-﻿namespace WebAppPrototype.Lib
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebAppPrototype.Lib
 {
     public class LapTimerMessageHandler
     {
+        private HttpClient m_httpClient;
+
+        public LapTimerMessageHandler()
+        {
+            m_httpClient = new HttpClient();
+        }
+
+        internal void Dispose()
+        {
+            m_httpClient.Dispose();
+        }
+
+        public Task<HttpResponseMessage> SendRaceStartNotification(IPAddress iPAddress, int countDownMillis)
+        {
+            JObject json = new JObject
+            {
+                { "type", "start" },
+                { "countDowmMillis", countDownMillis }
+            };
+            UriBuilder uriBuilder = new UriBuilder()
+            {
+                Host = iPAddress.ToString(),
+                Scheme = Uri.UriSchemeHttp
+            };
+            return m_httpClient.PostAsync(uriBuilder.ToString(), new StringContent(json.ToString()));
+        }
     }
 }
