421fd2eeb5de8e319d1bea8d801941bc7cd46330
John Wilson <john_wilson@trimble.com>
2020-04-12 13:53:26 -0500

    Commenting out integration test code for now

--- LibUnitTests/LapTimerMessageHandlerIntegrationTests.cs
+++ LibUnitTests/LapTimerMessageHandlerIntegrationTests.cs
@@ -22,13 +22,13 @@ namespace WebAppPrototype.LibUnitTests
             m_LapTimerMessageHandler.Dispose();
         }
 
-        [Fact]
-        public void SendRaceStartNotification()
-        {
-            string ipAddress = "127.0.0.1";
-            var message = m_LapTimerMessageHandler.SendRaceStartNotification(IPAddress.Parse(ipAddress), 5000);
-            message.Wait();
-            // assert ...
-        }
+        //[Fact]
+        //public void SendRaceStartNotification()
+        //{
+        //    string ipAddress = "127.0.0.1";
+        //    var message = m_LapTimerMessageHandler.SendRaceStartNotification(IPAddress.Parse(ipAddress), 5000);
+        //    message.Wait();
+        //    // assert ...
+        //}
     }
 }
